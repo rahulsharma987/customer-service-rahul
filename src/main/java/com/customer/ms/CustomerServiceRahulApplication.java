e741abeff4e4e78f193616783bf19ee56e9e925e
rahul <rahulmay987@gmail.com>
2020-09-30 13:52:48 -0400

    first exercise

---
+++ src/main/java/com/customer/ms/CustomerServiceRahulApplication.java
@@ -0,0 +1,13 @@
+package com.customer.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceRahulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceRahulApplication.class, args);
+	}
+
+}
